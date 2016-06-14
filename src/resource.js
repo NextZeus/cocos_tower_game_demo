50e743ce230f7f23aa4638d2663c7cc413935e62
xiaodong <xiaodong.li@asiainnovations.com>
2016-06-14 10:08:27 +0800

    cocos demo

---
+++ src/resource.js
@@ -0,0 +1,108 @@
+var res = {
+
+    load_png    : "res/load.png",
+    loadBar_png : "res/loadBar.png",
+
+	MainMenu_plist 		: "res/MainMenu.plist",
+	MainMenu_png 		: "res/MainMenu.png",
+
+	LeveInfo_plist 		: "res/LeveInfo.plist",
+	LeveInfo_png 		: "res/LeveInfo.png",
+
+	ChooseLevel_plist 	: "res/ChooseLevel.plist",
+	ChooseLevel_png 	: "res/ChooseLevel.png",
+
+	GamePlay_Info_plist : "res/GamePlay_Info.plist",
+	GamePlay_Info_png 	: "res/GamePlay_Info.png",
+    
+    enemy_plist 		: "res/enemy.plist",
+    enemy_png 			: "res/enemy.png",
+
+	sh_bg_png 			: "res/sh_bg.png",
+	go_failedPanel_png 	: "res/go_failedPanel.png",
+
+	startParticle_plist : "res/startParticle.plist",
+	startParticle_png 	: "res/startParticle.png",
+
+	ui_btnBack_png	: "res/ui_btnBack.png",
+	ui_btnBackNormal_png	: "res/ui_btnBackNormal.png",
+	ui_btnBackPress_png	: "res/ui_btnBackPress.png",
+	ui_btnNext_png	: "res/ui_btnNext.png",
+	ui_btnStart_png	: "res/ui_btnStart.png",
+
+	music_mp3 : "res/sound/music.mp3",
+	newEnemy_mp3 : "res/sound/comeout.wav"
+
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
+
+//g_resources.push("res/sh_bg.png");
+//
+//g_resources.push("res/ui_btnBack.png");
+//g_resources.push("res/ui_btnBackNormal.png");
+//g_resources.push("res/ui_btnBackPress.png");
+//g_resources.push("res/ui_btnNext.png");
+//g_resources.push("res/ui_btnStart.png");
+//
+//g_resources.push("res/mm_btnStartGame.png");
+//g_resources.push("res/mm_title.png");
+
+//
+//g_resources.push("res/cl_card_0.png");
+//g_resources.push("res/cl_card_1.png");
+//g_resources.push("res/cl_card_2.png");
+//g_resources.push("res/cl_pageMark_1.png");
+//g_resources.push("res/cl_pageMark_2.png");
+//g_resources.push("res/cl_selectLevelBg_0.png");
+//g_resources.push("res/cl_selectLevelBg_1.png");
+//g_resources.push("res/cl_selectLevelBg_2.png");
+//
+//g_resources.push("res/li_levelInfoPanel.png");
+//g_resources.push("res/li_star.png");
+//g_resources.push("res/li_starBg.png");
+//
+//g_resources.push("res/enemy.plist");
+//g_resources.push("res/enemy.png");
+//g_resources.push("res/gp_arrow.png");
+//g_resources.push("res/gp_arrowBullet.png");
+//g_resources.push("res/gp_arrowTowerInfo.png");
+//g_resources.push("res/gp_attackTower.png");
+//g_resources.push("res/gp_attackTowerInfo.png");
+//g_resources.push("res/gp_basePlate.png");
+//g_resources.push("res/gp_bullet.png");
+//g_resources.push("res/gp_hp.png");
+//g_resources.push("res/gp_hpBg.png");
+//g_resources.push("res/gp_multiDirTowerInfo.png");
+//g_resources.push("res/gp_playBg.jpg");
+//g_resources.push("res/gp_playHp.png");
+//g_resources.push("res/gp_playStar.png");
+//g_resources.push("res/gp_toolBg.png");
+//g_resources.push("res/gp_towerPos.png");
+//g_resources.push("res/gp_multiDirTower.png");
+//g_resources.push("res/gp_no.png");
+//g_resources.push("res/gp_noMoney.png");
+//g_resources.push("res/gp_gamePass.png");
+g_resources.push("res/levelInfo_0.plist");
+g_resources.push("res/levelInfo_1.plist");
+g_resources.push("res/levelInfo_2.plist");
+g_resources.push("res/levelInfo_3.plist");
+g_resources.push("res/tilemap0.tmx");
+g_resources.push("res/tilemap1.tmx");
+g_resources.push("res/tilemap2.tmx");
+g_resources.push("res/tilemap3.tmx");
+g_resources.push("res/tilemap4.tmx");
+g_resources.push("res/tilemap5.tmx");
+g_resources.push("res/tiles.png");
+//
+g_resources.push("res/fonts/bitmapFontChinese.fnt");
+g_resources.push("res/fonts/bitmapFontChinese.png");
+//
+//g_resources.push("res/go_failedPanel.png");
+
+g_resources.push("res/changjing.plist");
+
+
